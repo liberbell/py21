022862ca8e1dca6228fc74f97a7c535a3f709367
Liberbell <liberbell@gmail.com>
2020-06-24 08:54:10 +0900

    models1

--- myDjangoProjects/myMsgSite/myMsgApp/models.py
+++ myDjangoProjects/myMsgSite/myMsgApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=40, verbose_name="first name")
+    last_name = models.CharField(max_length=40)
+    
+    def __str__(self):
+        return self.name
