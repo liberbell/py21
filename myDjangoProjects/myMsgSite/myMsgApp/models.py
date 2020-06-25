e432c2acbc2dfac9efe703d823bb614471e72875
Liberbell <liberbell@gmail.com>
2020-06-25 09:48:47 +0900

    models4

--- myDjangoProjects/myMsgSite/myMsgApp/models.py
+++ myDjangoProjects/myMsgSite/myMsgApp/models.py
@@ -13,12 +13,12 @@ class Person(models.Model):
     def __str__(self):
         return "{0}".format(self.full_name)
 class Mail(models.Model):
-    MAILING_LIST_CHOISE = (
+    MAILING_LIST_CHOISES = (
                            ('GO', 'Gina Ortiz'),
                            ('RT', 'Rick Tallon'),
                            ('MF', 'Matt Foster'),
                            )
-    mail_to = models.CharField(max_length=2, choices=MAILING_LIST_CHOISE, default='GO', verbose_name='Send mail to')
+    mail_to = models.CharField(max_length=2, choices=MAILING_LIST_CHOISES, default='GO', verbose_name='Send mail to')
     subject = models.CharField(max_length=150, blank=False)
     mail_date = models.DateTimeField(auto_now=True)
     message = models.TextField(blank=False)
