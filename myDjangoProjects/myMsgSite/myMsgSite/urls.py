c28432d1a0da532f2d343bccab8d4e7aafb474f1
Liberbell <liberbell@gmail.com>
2020-06-27 16:45:30 +0900

    urls4

--- myDjangoProjects/myMsgSite/myMsgSite/urls.py
+++ myDjangoProjects/myMsgSite/myMsgSite/urls.py
@@ -22,5 +22,6 @@ from django.contrib.auth import views as admin_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', my_views.home, name='home'),
-    path('login/', my_views.Login.admin_view(), {'template_name':'login.html'}, name='mymsgsite_login'),
+    # path('login/', my_views.Login.admin_view(), {'template_name':'login.html'}, name='mymsgsite_login'),
+    path('login/', views.Login.as_view(), {'template_name':'login.html'}, name='mymsgsite_login'),
 ]
