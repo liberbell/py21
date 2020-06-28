c79d54f3f2c4394c7b715c2fb1b3b8e61c8d1097
Liberbell <liberbell@gmail.com>
2020-06-28 21:56:08 +0900

    views2

--- myDjangoProjects/myMsgSite/myMsgApp/views.py
+++ myDjangoProjects/myMsgSite/myMsgApp/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 def home(request):
     return render(request, "myMsgApp/home.html",
                 {'message_1':'Some content on Home page.',
-                }
+                 'message_2':'More detail on content of Home page.'
+                 })
                 #   {'h1Text': 'MessageBox',
                 #   'pText': 'Welcome to the Message Box App! Click Login button below to begin.'})
